4ab009cb37ac968d817a9561ab68a884f77c6431
imkindadone <lolaccmahdi1@gmail.com>
2025-09-02 10:00:37 -0700

    feat(SEO): robots+sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import robots from "astro-robots"; 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://sonoluminar.org",
+  integrations: [mdx(), sitemap(),robots()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
